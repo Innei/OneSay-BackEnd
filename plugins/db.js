c13cde83aa3277d8bf1c7cbe4733c0e7ab95be69
Innei <tukon479@gmail.com>
2019-08-03 14:40:30 +0800

    due to front-end

--- plugins/db.js
+++ plugins/db.js
@@ -1,7 +1,7 @@
 module.exports = app => {
   const mongoose = require('mongoose')
 
-  mongoose.connect('mongodb://localhost:27017/say_api', {
+  mongoose.connect('mongodb://localhost:27017/api', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useCreateIndex: true
--- routes/api/configs.js
+++ routes/api/configs.js
@@ -6,16 +6,20 @@ module.exports = app => {
   const router = express.Router({
     mergeParams: true
   })
+  const query = express.Router()
   router.get('/', async (req, res) => {
     const info = await Config.findOne({ name: 'information' })
     const times = await Config.findById(info.value.times.oid)
     info.value.times = times
     res.send(info)
   })
-  router.get('/my-info', async (req, res) => {
-    const info = await Config.findOne({ name: "my-info" })
-    
+
+  query.get('/:query', async (req, res) => {
+    const query = req.params.query
+    const info = await Config.findOne({ name: query })
     res.send(info)
   })
+  
+  app.use('/api/info/query', query)
   app.use('/api/info', router)
 }
